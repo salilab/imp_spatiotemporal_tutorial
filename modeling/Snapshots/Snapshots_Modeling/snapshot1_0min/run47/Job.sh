3ba8226973c2e63aef82369db73b3de8bf281fd8
miharozic <rozic8miha@gmail.com>
2024-08-12 18:26:54 +0200

    Adding snapshot1_0min
    Adding snapshot1_0min coiped from the Wynton. It includes all tests, Job.sh and also unrefined static_snapshot.py

---
+++ modeling/Snapshots/Snapshots_Modeling/snapshot1_0min/run47/Job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -r n
+#$ -j y
+#$ -N test1
+#$ -pe smp 16
+#$ -l h_rt=48:00:00
+
+module load Sali
+module load mpi/openmpi-x86_64
+module load imp
+module load python3/scikit/0.21.3
+
+mpirun -np $NSLOTS python3 static_snapshot.py 1 0min

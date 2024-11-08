1298e3bf58085c4f3f3804905a706a2ca60a326e
Ben Webb <ben@salilab.org>
2024-11-08 15:36:09 -0800

    We need pandas and sklearn to run tests

---
+++ support/setup_ci.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -e
+
+# Set up an environment to run tests under Travis CI (see toplevel .travis.yml)
+# or GitHub Actions (see toplevel .github/workflows/build.yml)
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 imp_branch python_version"
+  exit 1
+fi
+
+imp_branch=$1
+python_version=$2
+
+# get conda-forge, not main, packages
+conda config --remove channels defaults || :
+conda config --add channels conda-forge
+if [ ${imp_branch} = "develop" ]; then
+  IMP_CONDA="imp-nightly"
+else
+  IMP_CONDA="imp"
+fi
+
+if [ ${python_version} = "2.7" ]; then
+  pip="pip<=19.3.1"
+else
+  pip="pip"
+fi
+
+conda create --yes -q -n python${python_version} -c salilab python=${python_version} scipy matplotlib pandas scikit-learn ${pip} ${IMP_CONDA}
+eval "$(conda shell.bash hook)"
+conda activate python${python_version}
+
+if [ ${python_version} = "2.7" ]; then
+  # pytest-flake8 1.1.0 tries to import contextlib.redirect_stdout, which
+  # isn't present in Python 2
+  pip install pytest-cov coverage 'pytest-flake8<1.1'
+else
+  pip install pytest-cov coverage pytest-flake8
+fi
